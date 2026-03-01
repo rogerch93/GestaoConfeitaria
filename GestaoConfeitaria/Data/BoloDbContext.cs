7ae92bf28b573a4cce67b9eb5bfca9f514a6a7e6
Rogerio Chaves <rogerinhochaves@hotmail.com>
2026-03-01 07:39:26 -0300

    Criação DbContext

---
+++ GestaoConfeitaria/Data/BoloDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using GestaoConfeitaria.Models;
+
+namespace BoloGestaoAPI.Data;
+
+public class BoloDbContext : DbContext
+{
+    public BoloDbContext(DbContextOptions<BoloDbContext> options) : base(options) { }
+
+    public DbSet<Venda> Vendas { get; set; }
+    public DbSet<Material> MateriaisUsados { get; set; }
+    public DbSet<Gasto> Gastos { get; set; }
+}
