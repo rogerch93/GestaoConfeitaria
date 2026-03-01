775dbfdffdc7da23c322cdc614910913f2a6fade
Rogerio Chaves <rogerinhochaves@hotmail.com>
2026-03-01 07:39:07 -0300

    Criação das models

---
+++ GestaoConfeitaria/Models/Material.cs
@@ -0,0 +1,12 @@
+﻿namespace GestaoConfeitaria.Models
+{
+    public class Material
+    {
+        public int Id { get; set; }
+        public int VendaId { get; set; }
+        public string Nome { get; set; } = "";
+        public decimal Quantidade { get; set; }
+        public decimal CustoUnitario { get; set; }
+        public decimal CustoTotal => Quantidade * CustoUnitario;
+    }
+}
