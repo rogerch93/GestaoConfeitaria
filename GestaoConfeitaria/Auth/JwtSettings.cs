643d84eeb007bd2f8801e37174f6a4b1eba6ccd6
Rogerio Chaves <rogerinhochaves@hotmail.com>
2026-03-01 07:41:05 -0300

    Criação - Autenticações

---
+++ GestaoConfeitaria/Auth/JwtSettings.cs
@@ -0,0 +1,9 @@
+namespace MeuProjetoIA.Auth;
+
+public class JwtSettings
+{
+    public string SecretKey { get; set; } = string.Empty;
+    public string Issuer { get; set; } = "GestaoConfeitaria";
+    public string Audience { get; set; } = "GestaoConfeitaria";
+    public int ExpiresInMinutes { get; set; } = 60; //1 hora
+}
