3288ecd7378fe6bf38a5e3407b2cb900ef7b559d
Rogerio Chaves <rogerinhochaves@hotmail.com>
2026-03-01 07:41:41 -0300

    Configuração de pipeline

--- GestaoConfeitaria/Program.cs
+++ GestaoConfeitaria/Program.cs
@@ -1,11 +1,22 @@
+using BoloGestaoAPI.Data;
+using GestaoConfeitaria.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<BoloDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpClient();
+builder.Services.AddScoped<GroqService>();
 
 var app = builder.Build();
 
