3b98d81f7c9ee53c42ae494bbdcf9c5483fa179b
Rogerio Chaves <rogerinhochaves@hotmail.com>
2026-03-03 10:32:45 -0300

    Migração do middleware da chave de api

---
+++ GestaoConfeitaria/Middleware/ApiKeyMiddleware.cs
@@ -0,0 +1,49 @@
+﻿namespace GestaoConfeitaria.Middleware
+{
+    public class ApiKeyMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private const string ApiKeyHeaderName = "X-API-Key";
+
+        public ApiKeyMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var path = context.Request.Path.Value?.TrimEnd('/').ToLowerInvariant() ?? string.Empty;
+            //Endpoints publicos não necessitam de API Key
+            if (context.Request.Path.StartsWithSegments("/health") ||
+            context.Request.Path.StartsWithSegments("/debug-env") ||
+            context.Request.Path.StartsWithSegments("/api/auth/login") ||
+            context.Request.Path.StartsWithSegments("/swagger"))
+            {
+                await _next(context);
+                return;
+            }
+
+            //Pegar chave esperada do configuration
+            if (!context.Request.Headers.TryGetValue(ApiKeyHeaderName, out var extractedApiKey))
+            {
+                context.Response.StatusCode = 401; // Unauthorized
+                await context.Response.WriteAsync("Chave API Invalida ou ausente.");
+                return;
+            }
+
+            //Pegar a chave esperada do configuration
+            var appSettingsApiKey = context.RequestServices
+                .GetRequiredService<IConfiguration>()
+                .GetValue<string>("ApiKey");
+
+            if (appSettingsApiKey == null || !extractedApiKey.Equals(appSettingsApiKey))
+            {
+                context.Response.StatusCode = 401; // Unauthorized
+                await context.Response.WriteAsync("Chave API Invalida ou ausente.");
+                return;
+            }
+
+            await _next(context);
+        }
+    }
+}
