1b9b5d1bf8d5c1ae1cf1e1674a02f554c2cd19ce
Rogerio Chaves <rogerinhochaves@hotmail.com>
2026-03-08 09:06:43 -0300

    Criação de nova coluna DataExclusao

---
+++ GestaoConfeitaria/Migrations/20260306142820_CriaçãoColunasExclusao.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestaoConfeitaria.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriaçãoColunasExclusao : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataExclusao",
+                table: "Vendas",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataExclusao",
+                table: "Users",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataExclusao",
+                table: "MateriaisUsados",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataExclusao",
+                table: "Gastos",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DataExclusao",
+                table: "Vendas");
+
+            migrationBuilder.DropColumn(
+                name: "DataExclusao",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "DataExclusao",
+                table: "MateriaisUsados");
+
+            migrationBuilder.DropColumn(
+                name: "DataExclusao",
+                table: "Gastos");
+        }
+    }
+}
