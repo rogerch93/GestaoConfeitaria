ec263efd5f9416cf6021eb89b7b18d63ce6e641c
Rogerio Chaves <rogerinhochaves@hotmail.com>
2026-03-10 21:34:01 -0300

    Criação de Biblioteca de classes para evitar repetição de models

---
+++ GestaoConfeitariaBiblioteca/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace GestaoConfeitariaBiblioteca.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public string Role { get; set; } = "User";
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? DataExclusao { get; set; } = DateTime.UtcNow;
+    }
+}
