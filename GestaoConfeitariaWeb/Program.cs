057311a17d491d8ea5accb15cea417d48fd358c6
Rogerio Chaves <rogerinhochaves@hotmail.com>
2026-03-12 22:05:35 -0300

    JWT nao esta dando certo com Blazor, ajuste para autenticação por cookies

--- GestaoConfeitariaWeb/Program.cs
+++ GestaoConfeitariaWeb/Program.cs
@@ -2,54 +2,65 @@ using Blazored.LocalStorage;
 using GestaoConfeitariaWeb.Auth;
 using GestaoConfeitariaWeb.Components;
 using GestaoConfeitariaWeb.Service;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Components.Authorization;
-using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Components.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 builder.Services.AddScoped<VendaServices>();
+
+// LocalStorage para guardar o token JWT
 builder.Services.AddBlazoredLocalStorage();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.Cookie.HttpOnly = true;
+        options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+        options.Cookie.SameSite = SameSiteMode.Strict;
+        options.LoginPath = "/login";
+        options.AccessDeniedPath = "/access-denied";
+        options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
+    });
+
+builder.Services.AddAuthorization();
+
+// Autenticação custom (CustomAuthStateProvider)
 builder.Services.AddScoped<CustomAuthStateProvider>();
 builder.Services.AddScoped<AuthenticationStateProvider>(sp => sp.GetRequiredService<CustomAuthStateProvider>());
+
+// Suporte a [Authorize] em componentes
 builder.Services.AddAuthorizationCore();
+
+//Handler que injeta o token dinamicamente em TODAS as chamadas do HttpClient nomeado
 builder.Services.AddScoped<JwtAuthorizationMessageHandler>();
-builder.Services.AddHttpClient("Api", client => client.BaseAddress = new Uri("https://localhost:7230/"))
-    .AddHttpMessageHandler<JwtAuthorizationMessageHandler>();
 
-// HttpClient com token interceptor
-builder.Services.AddScoped(sp =>
-{
-    var client = new HttpClient { BaseAddress = new Uri("https://localhost:7230/") };
-    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", /* token de localStorage? */);
-    // Melhor: crie um DelegatingHandler para injetar token dinamicamente
-    return client;
-});
-
-//porta da API
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7230/") });
-
+// HttpClient nomeado e único para a API
 builder.Services.AddHttpClient("Api", client =>
 {
-    client.BaseAddress = new Uri("https://localhost:7230/");
+    client.BaseAddress = new Uri("https://localhost:7230/");  //porta da API
     client.DefaultRequestHeaders.Add("Accept", "application/json");
-});
+})
+.AddHttpMessageHandler<JwtAuthorizationMessageHandler>();     // injeta token automaticamente
+
+// Acesso ao HttpContext no servidor, útil para claims ou cookies
+builder.Services.AddHttpContextAccessor();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Pipeline HTTP
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 app.UseAntiforgery();
 
