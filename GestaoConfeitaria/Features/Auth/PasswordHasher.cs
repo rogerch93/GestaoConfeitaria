3853bef4e80783fd6e77f8781f944b0539a6213d
Rogerio Chaves <rogerinhochaves@hotmail.com>
2026-03-19 08:57:41 -0300

    Criação de nova feature de autenticação

---
+++ GestaoConfeitaria/Features/Auth/PasswordHasher.cs
@@ -0,0 +1,16 @@
+﻿using BCrypt.Net;
+
+namespace GestaoConfeitaria.Features.Auth;
+public static class PasswordHasher
+{
+    public static string HashPassword(string password)
+    {
+        return BCrypt.Net.BCrypt.HashPassword(password);
+    }
+
+    public static bool VerifyPassword(string password, string hash)
+    {
+        return BCrypt.Net.BCrypt.Verify(password, hash);
+    }
+}
+
